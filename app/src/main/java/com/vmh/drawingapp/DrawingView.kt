ec3201b9bd7b5010a21d19b6ecf7e663625c1e1e
vamsimadhav <vamsimadhav2000@gmail.com>
2023-08-01 20:15:42 +0530

    Added the color tray and able to change brush color

--- app/src/main/java/com/vmh/drawingapp/DrawingView.kt
+++ app/src/main/java/com/vmh/drawingapp/DrawingView.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -34,7 +35,6 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -93,6 +93,18 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
         }
         invalidate()
         return true
+    }
+
+    fun setBrushSize(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,newSize,resources.displayMetrics)
+
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    fun setBrushColor(newColor: String){
+        val color = Color.parseColor(newColor)
+        mBrushColor = color
+//        mDrawPaint!!.color = color
     }
 
     internal inner class CustomPath(var color: Int,var brushThickness: Float): Path() {
--- app/src/main/java/com/vmh/drawingapp/MainActivity.kt
+++ app/src/main/java/com/vmh/drawingapp/MainActivity.kt
@@ -1,11 +1,85 @@
 package com.vmh.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import com.vmh.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawingView: DrawingView
+    private lateinit var brushSelector: ImageButton
+    private lateinit var colorTray: LinearLayout
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mCurrentSelectedPaintButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        drawingView = binding.drawingSurface
+        brushSelector = binding.brushSizeSelector
+        colorTray = binding.colorTray
+        mCurrentSelectedPaintButton = colorTray[1] as ImageButton
+
+        drawingView.setBrushSize(20f)
+        brushSelector.setOnClickListener { showBrushSizeDialog() }
+
+        mCurrentSelectedPaintButton.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.color_tray_pressed
+            )
+        )
+    }
+
+     fun setBrushColor(view: View){
+        if(view !== mCurrentSelectedPaintButton){
+            val imageButton = view as ImageButton
+            val color = imageButton.tag.toString()
+            drawingView.setBrushColor(color)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.color_tray_pressed)
+            )
+
+            mCurrentSelectedPaintButton.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.color_tray)
+            )
+
+            mCurrentSelectedPaintButton = view
+        }
+    }
+
+    private fun showBrushSizeDialog(){
+        val brushSelectionDialog = Dialog(this)
+        brushSelectionDialog.setContentView(R.layout.brush_selection_dialog)
+        brushSelectionDialog.setTitle("Brush Selector")
+
+        val smallBrush: ImageButton = brushSelectionDialog.findViewById(R.id.smallBrush)
+        val medBrush: ImageButton = brushSelectionDialog.findViewById(R.id.medBrush)
+        val largeBrush: ImageButton = brushSelectionDialog.findViewById(R.id.largeBrush)
+
+        smallBrush.setOnClickListener {
+            drawingView.setBrushSize(10f)
+            brushSelectionDialog.dismiss()
+        }
+
+        medBrush.setOnClickListener {
+            drawingView.setBrushSize(20f)
+            brushSelectionDialog.dismiss()
+        }
+
+        largeBrush.setOnClickListener {
+            drawingView.setBrushSize(30f)
+            brushSelectionDialog.dismiss()
+        }
+
+        brushSelectionDialog.show()
     }
 }
